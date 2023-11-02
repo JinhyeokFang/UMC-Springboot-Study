2144e7481c15d96cc9ea822721ed5bd109b69809
JinhyeokBang <jinhyeokfang@gmail.com>
2023-11-03 00:59:42 +0900

    chore: init

---
+++ src/test/java/uk/jinhy/umcstudy/UmcStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jinhy.umcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
