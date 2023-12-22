486cef9d117dafd8de2061ab9e938654e5efafff
JinhyeokBang <jinhyeokfang@gmail.com>
2023-12-22 17:31:38 +0900

    fix: error

--- src/main/java/uk/jinhy/umcstudy/UmcStudyApplication.java
+++ src/main/java/uk/jinhy/umcstudy/UmcStudyApplication.java
@@ -2,7 +2,6 @@ package uk.jinhy.umcstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class UmcStudyApplication {
